a43cf2e77758a0ecfe991db1e2fa00e32cfdf9b1
Your-GPT <kontakt@your-gpt.de>
2024-12-05 19:38:52 +0100

    Create g-portal.js

---
+++ g-portal.js
@@ -0,0 +1,18 @@
+(function() {
+    function loadScript(src, callback) {
+        const script = document.createElement('script');
+        script.src = src;
+        script.async = true;
+        script.onload = () => callback && callback();
+        script.onerror = () => console.error(`Failed to load script: ${src}`);
+        document.body.appendChild(script);
+    }
+
+    loadScript('https://your-gpt.github.io/widget/config.js', () => {
+        loadScript(config.injectUrl, () => {
+            loadScript('https://files.bpcontent.cloud/2024/12/05/18/20241205182205-PHBBFK73.js', () => {
+                // Weitere Aktionen nach dem Laden des Skripts
+            });
+        });
+    });
+})();
