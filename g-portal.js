b33d760b68da3d507c8068ddc3dac03c7f1998e4
Your-GPT <kontakt@your-gpt.de>
2024-12-05 19:44:07 +0100

    Update g-portal.js

--- g-portal.js
+++ g-portal.js
@@ -10,7 +10,7 @@
 
     loadScript('https://your-gpt.github.io/widget/config.js', () => {
         loadScript(config.injectUrl, () => {
-            loadScript('https://files.bpcontent.cloud/2024/12/05/18/20241205182205-PHBBFK73.js', () => {
+            loadScript('https://files.bpcontent.cloud/2024/12/05/18/20241205184019-RFYJSDP4.js', () => {
                 // Weitere Aktionen nach dem Laden des Skripts
             });
         });
