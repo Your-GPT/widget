ab3c28f16660744ac814941640f8fed446201fc9
Your-GPT <kontakt@your-gpt.de>
2025-01-23 21:55:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 const config = {
-    injectUrl: 'https://cdn.botpress.cloud/webchat/v2.2/inject.js'
+    injectUrl: 'https://cdn.botpress.cloud/webchat/v2.2/inject.js',
+    widget: window.botpress // Store the reference here
 };
